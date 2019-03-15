60656a7126d2da7e04a87d3cd1686d4a735c9adb
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2019-03-15 14:42:46 +0900

    create .tmux.conf

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -4,3 +4,4 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
