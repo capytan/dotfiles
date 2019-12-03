9fe956fb9524e3508838f64cc865b5bc94de5d6e
Yosuke Mizuguchi <mizuguchi.yosuke.fcp@gmail.com>
2019-12-03 19:40:33 +0900

    Add VSCode

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/ApplicationSupport/Code/User/settings.json
