8bebdd51ef65a58c734927922c12cd2723044d59
capytan <mizuguchi.yosuke.fcp@gmail.com>
2022-05-21 07:23:25 +0900

    Add: ubuntu

---
+++ ubuntu/dotfileslink.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf ~/dotfiles/ubuntu/.alacritty.yml ~/.alacritty.yml
+ln -sf ~/dotfiles/ubuntu/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/ubuntu/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/ubuntu/init.vim ~/.config/nvim/init.vim
