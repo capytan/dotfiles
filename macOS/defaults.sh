f0bd62c1ef3a056de7271462c480d1c137b7333d
capytan(Yosuke Mizuguchi) <yosuke.mizuguchi@studyplus.jp>
2022-06-03 15:38:30 +0900

    Update: .git-prompt.sh defaults.sh

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -5,3 +5,10 @@ defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
 # Do not write RAM backup during sleep
 # See: https://aotamasaki.hatenablog.com/entry/intelmac_crash_during_sleep
 sudo pmset hibernatemode 0
+
+# hidden
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+# vscode
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
