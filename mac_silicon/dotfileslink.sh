5c48e6c6ea3c27bfb9d3b661d6f37715299acbd8
capytan <mizuguchi.yosuke.fcp@gmail.com>
2024-01-24 03:10:38 +0900

    add(macOS): tmux.conf

--- mac_silicon/dotfileslink.sh
+++ mac_silicon/dotfileslink.sh
@@ -2,3 +2,4 @@
 ln -sf ~/dotfiles/mac_silicon/.fzf.zsh ~/.fzf.zsh
 ln -sf ~/dotfiles/mac_silicon/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/mac_silicon/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/macOS/.tmux.conf ~/.tmux.conf
