5450b560168ad6f3a4d7b3bc52fb5670e63d44be
capytan <mizuguchi.yosuke.fcp@gmail.com>
2024-06-19 23:47:27 +0900

    fix: dotfiles導入用シェルスクリプトを更新

--- macos/dotfileslink.sh
+++ macos/dotfileslink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/mac_silicon/.fzf.zsh ~/.fzf.zsh
-ln -sf ~/dotfiles/mac_silicon/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/mac_silicon/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/macOS/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/macos/.fzf.zsh ~/.fzf.zsh
+ln -sf ~/dotfiles/macos/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/macos/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/macos/.zshrc ~/.zshrc
