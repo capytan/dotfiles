bd9e980745b3f0dfe7683f5de15e5bf86e72c270
capytan <mizuguchi.yosuke.fcp@gmail.com>
2024-06-19 23:55:01 +0900

    chore: 使用していないinit.vimを削除

--- macos_intel/dotfileslink.sh
+++ macos_intel/dotfileslink.sh
@@ -4,7 +4,6 @@ ln -sf ~/dotfiles/macos_intel/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/macos_intel/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/macos_intel/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/macos_intel/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/macos_intel/init.vim ~/.config/nvim/init.vim
 
 # ln -sf ~/dotfiles/macos_intel/.zprofile ~/.zprofile
 # ln -sf ~/dotfiles/macos_intel/vscode/settings.json ~/Library/ApplicationSupport/Code/User/settings.json
