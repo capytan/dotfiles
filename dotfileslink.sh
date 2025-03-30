4a693d563939ef0cb4347ce6309ed6814f882f07
capytan <mizuguchi.yosuke.fcp@gmail.com>
2025-03-30 17:02:08 +0900

    chore: add symbolic links for macOS config files

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -5,5 +5,5 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/ApplicationSupport/Code/User/settings.json
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/init.vim ~/.config/nvim/init.vim
--- macos/dotfileslink.sh
+++ macos/dotfileslink.sh
@@ -5,3 +5,4 @@ ln -sf ~/dotfiles/macos/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/macos/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/macos/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
+ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
