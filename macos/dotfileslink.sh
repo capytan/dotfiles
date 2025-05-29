dca58bd8518d63c087047f1cb5ac4dc06c092f7c
capytan <yosuke_mizuguchi@smartbank.co.jp>
2025-05-29 20:23:49 +0900

    feat(alacritty): add initial Alacritty config files and linking script

--- macos/dotfileslink.sh
+++ macos/dotfileslink.sh
@@ -7,3 +7,9 @@ ln -sf ~/dotfiles/macos/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Cursor/User/settings.json
 ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -sf ~/dotfiles/mise/config.toml ~/.config/mise/config.toml
+mkdir -p ~/.config/alacritty/fonts
+ln -sf ~/dotfiles/configs/alacritty.toml ~/.config/alacritty.toml
+ln -sf ~/dotfiles/configs/alacritty/shared.toml ~/.config/alacritty/shared.toml
+ln -sf ~/dotfiles/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
+ln -sf ~/dotfiles/configs/alacritty/pane.toml ~/.config/alacritty/pane.toml
+ln -sf ~/dotfiles/configs/alacritty/fonts/JetBrainsMono.toml ~/.config/alacritty/fonts/JetBrainsMono.toml
